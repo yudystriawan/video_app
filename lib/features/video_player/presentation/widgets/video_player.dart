7b6d770972de902b579e984eb8d3c1f961dfeb33
yudystriawan <yudistriawan@gmail.com>
2023-02-25 19:03:38 +0700

    fix videoplayer

--- lib/features/video_player/presentation/widgets/video_player.dart
+++ lib/features/video_player/presentation/widgets/video_player.dart
@@ -223,7 +223,7 @@ class _VideoPlayerState extends State<VideoPlayer> {
                     )
                 ] else ...[
                   playerWidget,
-                  Padding(
+                  SingleChildScrollView(
                     padding: const EdgeInsets.all(16.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
