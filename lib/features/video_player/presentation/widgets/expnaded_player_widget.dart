b3043bf3ffc760348d5399a418caadd5150403d2
yudystriawan <yudistriawan@gmail.com>
2023-07-05 17:44:20 +0800

    fix imports

--- lib/features/video_player/presentation/widgets/expnaded_player_widget.dart
+++ lib/features/video_player/presentation/widgets/expnaded_player_widget.dart
@@ -1,7 +1,7 @@
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:video_app/features/video_player/presentation/widgets/video_screen.dart';
+import 'video_screen.dart';
 
 import '../../../../core/utils/util.dart';
 import '../bloc/mini_player/mini_player_bloc.dart';
