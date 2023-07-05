13bada0ec362de19e7bd255e34265afe6ab8d254
yudystriawan <yudistriawan@gmail.com>
2023-07-05 17:15:56 +0800

    refactor widgets

--- lib/features/video_player/presentation/pages/video_detail_page.dart
+++ lib/features/video_player/presentation/pages/video_detail_page.dart
@@ -3,12 +3,11 @@ import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:miniplayer/miniplayer.dart';
-import 'package:video_player/video_player.dart';
 
-import '../../../../core/utils/util.dart';
 import '../bloc/mini_player/mini_player_bloc.dart';
 import '../bloc/video_player/video_player_bloc.dart';
-import '../widgets/video_screen.dart';
+import '../widgets/expnaded_player_widget.dart';
+import '../widgets/mini_player_widget.dart';
 
 @RoutePage()
 class VideoDetailPage extends StatelessWidget {
@@ -38,225 +37,18 @@ class VideoDetailPage extends StatelessWidget {
                 final maxPlayerSize = width * 0.4;
 
                 if (!isMiniplayer) {
-                  return FullSizeDetailedPlayer(
+                  return ExpandedPlayerWidget(
                     height: height,
                     maxPlayerSize: maxPlayerSize,
                     controller: chewieController,
                   );
                 }
-                return MiniDetailedPlayer(
+                return MiniPlayerWidget(
                   height: height,
                   maxPlayerSize: maxPlayerSize,
                   controller: chewieController.copyWith(showControls: false),
                 );
               },
-            );
-          },
-        );
-      },
-    );
-  }
-}
-
-class FullSizeDetailedPlayer extends StatelessWidget {
-  const FullSizeDetailedPlayer({
-    Key? key,
-    required this.height,
-    required this.maxPlayerSize,
-    this.controller,
-  }) : super(key: key);
-
-  final double height;
-  final double maxPlayerSize;
-  final ChewieController? controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<MiniPlayerBloc, MiniPlayerState>(
-      builder: (context, state) {
-        final width = MediaQuery.of(context).size.width;
-
-        var percentageExpandedPlayer = percentageFromValueInRange(
-          min: state.playerMaxHeight * miniplayerPercentageDeclaration +
-              state.playerMinHeight,
-          max: state.playerMaxHeight,
-          value: height,
-        );
-        if (percentageExpandedPlayer < 0) percentageExpandedPlayer = 0;
-        final paddingVertical = valueFromPercentageInRange(
-            min: 0, max: 10, percentage: percentageExpandedPlayer);
-        final double heightWithoutPadding = height - paddingVertical * 2;
-        final double playerheight = heightWithoutPadding > maxPlayerSize
-            ? maxPlayerSize
-            : heightWithoutPadding;
-
-        return Column(
-          children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: VideoScreen(
-                width: width,
-                height: playerheight,
-                controller: controller,
-              ),
-            ),
-            BlocBuilder<VideoPlayerBloc, VideoPlayerState>(
-              buildWhen: (p, c) => p.currentVideo != c.currentVideo,
-              builder: (context, state) {
-                return Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 33),
-                    child: Opacity(
-                      opacity: percentageExpandedPlayer,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Flexible(
-                            child: Text(state.currentVideo?.title ?? '-'),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-}
-
-class MiniDetailedPlayer extends StatelessWidget {
-  const MiniDetailedPlayer({
-    Key? key,
-    required this.height,
-    required this.maxPlayerSize,
-    this.controller,
-  }) : super(key: key);
-
-  final double height;
-  final double maxPlayerSize;
-  final ChewieController? controller;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<MiniPlayerBloc, MiniPlayerState>(
-      builder: (context, state) {
-        final playerMinHeight = state.playerMinHeight;
-        final playerMaxHeight = state.playerMaxHeight;
-
-        final percentageMiniplayer = percentageFromValueInRange(
-          min: playerMinHeight,
-          max: playerMaxHeight * miniplayerPercentageDeclaration +
-              playerMinHeight,
-          value: height,
-        );
-
-        final elementOpacity = 1 - 1 * percentageMiniplayer;
-        final progressIndicatorHeight = 4 - 4 * percentageMiniplayer;
-
-        return BlocBuilder<VideoPlayerBloc, VideoPlayerState>(
-          buildWhen: (p, c) => p.currentVideo != c.currentVideo,
-          builder: (context, state) {
-            final currentVideo = state.currentVideo;
-
-            return Column(
-              children: [
-                Expanded(
-                  child: Row(
-                    children: [
-                      VideoScreen(
-                        width: maxPlayerSize,
-                        height: maxPlayerSize + progressIndicatorHeight,
-                        showControl: false,
-                        controller: controller,
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.only(left: 10),
-                          child: Opacity(
-                            opacity: elementOpacity,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(currentVideo?.title ?? '-'),
-                                Text(currentVideo?.subtitle ?? '-'),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.close),
-                        onPressed: () => context
-                            .read<VideoPlayerBloc>()
-                            .add(const VideoPlayerEvent.stopped()),
-                      ),
-                      ValueListenableBuilder(
-                        valueListenable: context
-                            .watch<VideoPlayerBloc>()
-                            .videoPlayerController!,
-                        builder: (context, value, child) {
-                          final isPlaying = value.isPlaying;
-
-                          return Padding(
-                            padding: const EdgeInsets.only(right: 3),
-                            child: Opacity(
-                              opacity: elementOpacity,
-                              child: IconButton(
-                                icon: Icon(isPlaying
-                                    ? Icons.pause_circle_filled
-                                    : Icons.play_circle_filled),
-                                onPressed: () {
-                                  if (isPlaying) {
-                                    context
-                                        .read<VideoPlayerBloc>()
-                                        .add(const VideoPlayerEvent.paused());
-                                    return;
-                                  }
-
-                                  context
-                                      .read<VideoPlayerBloc>()
-                                      .add(const VideoPlayerEvent.resumed());
-                                },
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: progressIndicatorHeight,
-                  child: Opacity(
-                    opacity: elementOpacity,
-                    child: ValueListenableBuilder(
-                      valueListenable: context
-                          .watch<VideoPlayerBloc>()
-                          .videoPlayerController!,
-                      builder: (
-                        BuildContext context,
-                        VideoPlayerValue value,
-                        Widget? child,
-                      ) {
-                        final durationLinear = percentageFromDuration(
-                          value.position,
-                          Duration.zero,
-                          value.duration,
-                        );
-                        return LinearProgressIndicator(
-                          value: durationLinear,
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ],
             );
           },
         );
