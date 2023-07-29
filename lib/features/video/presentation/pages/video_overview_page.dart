907ae7744c6b579c62df1a2b23b868938ed1d975
yudystriawan <yudistriawan@gmail.com>
2023-07-29 08:58:52 +0700

    update app bar

--- lib/features/video/presentation/pages/video_overview_page.dart
+++ lib/features/video/presentation/pages/video_overview_page.dart
@@ -4,6 +4,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:video_app/shared/widgets/app_bar.dart';
 
+import '../../../../routes/router.dart';
+import '../../../../shared/widgets/icon.dart';
 import '../bloc/mini_player/mini_player_bloc.dart';
 import '../bloc/video_player/video_player_bloc.dart';
 import '../widgets/video_list_widget.dart';
@@ -28,7 +30,35 @@ class VideoOverviewPage extends StatelessWidget {
                     children: [
                       Column(
                         children: [
-                          const MyAppBar(),
+                          MyAppBar(
+                            title: const Text('Video'),
+                            trailing: Row(
+                              children: [
+                                AppIcon(
+                                  icon: const Icon(
+                                      Icons.notifications_none_outlined),
+                                  onTap: () {},
+                                ),
+                                SizedBox(
+                                  width: 12.w,
+                                ),
+                                AppIcon(
+                                  icon: const Icon(Icons.search),
+                                  onTap: () => context.pushRoute(SearchRoute()),
+                                ),
+                                SizedBox(
+                                  width: 12.w,
+                                ),
+                                Container(
+                                  width: 24.w,
+                                  height: 24.w,
+                                  decoration: const BoxDecoration(
+                                      color: Colors.grey,
+                                      shape: BoxShape.circle),
+                                ),
+                              ],
+                            ),
+                          ),
                           Divider(
                             indent: 12.w,
                             endIndent: 12.w,
--- lib/shared/widgets/app_bar.dart
+++ lib/shared/widgets/app_bar.dart
@@ -1,45 +1,48 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:video_app/routes/router.dart';
 import 'package:video_app/shared/widgets/icon.dart';
 
 class MyAppBar extends StatelessWidget {
-  const MyAppBar({super.key});
+  const MyAppBar({
+    Key? key,
+    required this.title,
+    this.trailing,
+    this.leading,
+  }) : super(key: key);
+
+  final Widget title;
+  final Widget? trailing;
+  final Widget? leading;
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 338.w,
+    final hasParent = context.router.canPop();
+
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 12.w),
       height: 48.w,
       child: Row(
         children: [
-          const Expanded(
-            child: Text('VideoApp'),
+          if (hasParent) ...[
+            leading ??
+                AppIcon(
+                  icon: const Icon(Icons.arrow_back),
+                  onTap: () => context.router.pop(),
+                ),
+            SizedBox(
+              width: 12.w,
+            ),
+          ],
+          Expanded(
+            child: title,
           ),
-          SizedBox(
-            width: 12.w,
-          ),
-          AppIcon(
-            icon: const Icon(Icons.notifications_none_outlined),
-            onTap: () {},
-          ),
-          SizedBox(
-            width: 12.w,
-          ),
-          AppIcon(
-            icon: const Icon(Icons.search),
-            onTap: () => context.pushRoute(SearchRoute()),
-          ),
-          SizedBox(
-            width: 12.w,
-          ),
-          Container(
-            width: 24.w,
-            height: 24.w,
-            decoration:
-                const BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
-          ),
+          if (trailing != null) ...{
+            SizedBox(
+              width: 12.w,
+            ),
+            trailing!,
+          },
         ],
       ),
     );
